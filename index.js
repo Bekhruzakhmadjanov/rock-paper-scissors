417564f442335f86e6c371d5ccf068c31829860c
Volonter <volonter@uzinfocom.local>
2023-07-05 09:16:41 +0500

    Change index.js

--- index.js
+++ index.js
@@ -1,25 +1,97 @@
-const computerSelection = getComputerChoice();
+// const computerSelection = getComputerChoice();
+// const buttons = document.querySelectorAll('input');
+// let playerScore = 0;
+// let computerScore = 0;
+
+// function getComputerChoice() {
+//     let choice =  Math.floor(Math.random() * (3) + 1);
+//     if(choice === 1)
+//     {
+//         return "rock";
+//     }
+
+//     else if(choice === 2)
+//     {
+//         return "paper";
+//     }
+
+//     else
+//     {
+//         return "scissor";
+//     }
+    
+// }
+
+
+// function playRound(playerSelection) {
+//     playerSelection = playerSelection.toLowerCase();
+//     let result = "";
+//     // your code here!
+//     if ( (playerSelection == "rock" && computerSelection == "paper") 
+//     || (playerSelection == "paper" && computerSelection == "scissor") 
+//     || (playerSelection == "scissor" && computerSelection == "rock") ) {
+//         alert("You lose!");
+//         computerScore++;
+//         result = ('You lose! ' + playerSelection + ' beats ' + computerSelection
+//             + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+
+//         if(computerScore == 5) {
+//             result += '<br><br>You lost the game! Reload the page to play again';
+//             buttons.forEach(elem => {
+//                 elem.disabled = true
+//             })
+//         }
+//     }
+
+//     else if (playerSelection == computerSelection) {
+//         alert("Tie");
+//         result = ('Tie! ' + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+//     }
+
+//     else {
+//         alert("You win!");
+//         playerScore++;
+//         result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+//             + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
+
+//         if(playerScore == 5) {
+//             result += '<br><br>You win the game! Reload the page to play again';
+//             buttons.forEach(elem => {
+//                 elem.disabled = true
+//             })
+//         }
+//     }
+
+//     document.getElementById('result').innerHTML = result
+//     return result;
+// }
+
+
+// buttons.forEach(button =>{
+//     button.addEventListener('click', function(){
+//         playRound(button.value)
+//     })
+// })
+
+
+
+
+
+
+
+let computerSelection = getComputerChoice();
 const buttons = document.querySelectorAll('input');
 let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let choice =  Math.floor(Math.random() * (3) + 1);
-    if(choice === 1)
-    {
-        return "rock";
-    }
-
-    else if(choice === 2)
-    {
-        return "paper";
-    }
-
-    else
-    {
-        return "scissor";
-    }
-    
+    let choice =  Math.ceil(Math.random() * 3);
+    switch (choice) {
+        case 1: return 'rock';
+        case 2: return 'paper';
+        case 3: return 'scissor';
+        default: return null;
+    }    
 }
 
 
@@ -30,12 +102,11 @@ function playRound(playerSelection) {
     if ( (playerSelection == "rock" && computerSelection == "paper") 
     || (playerSelection == "paper" && computerSelection == "scissor") 
     || (playerSelection == "scissor" && computerSelection == "rock") ) {
-        alert("You lose!");
         computerScore++;
         result = ('You lose! ' + playerSelection + ' beats ' + computerSelection
             + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
 
-        if(computerScore == 5) {
+        if(computerScore === 5) {
             result += '<br><br>You lost the game! Reload the page to play again';
             buttons.forEach(elem => {
                 elem.disabled = true
@@ -43,7 +114,7 @@ function playRound(playerSelection) {
         }
     }
 
-    else if (playerSelection == computerSelection) {
+    else if (playerSelection === computerSelection) {
         alert("Tie");
         result = ('Tie! ' + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
     }
@@ -54,7 +125,7 @@ function playRound(playerSelection) {
         result = ('You win! ' + playerSelection + ' beats ' + computerSelection
             + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore);
 
-        if(playerScore == 5) {
+        if(playerScore === 5) {
             result += '<br><br>You win the game! Reload the page to play again';
             buttons.forEach(elem => {
                 elem.disabled = true
@@ -66,9 +137,9 @@ function playRound(playerSelection) {
     return result;
 }
 
-
 buttons.forEach(button =>{
     button.addEventListener('click', function(){
+        computerSelection = getComputerChoice();
         playRound(button.value)
     })
 })
